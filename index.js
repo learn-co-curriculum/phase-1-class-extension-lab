cd205cac77284a59f2a08bd27f56c5e22f94f30e
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-12-04 18:08:38 -0500

    Completes solution and tests

--- index.js
+++ index.js
@@ -5,19 +5,31 @@ class Shape {
 }
 
 class Triangle {
+  constructor(length) {
+    this.length = length
+  }
+
   perimeter(){
     return this.length * 3
   }
 }
 
 class Square {
+  constructor(length) {
+    this.length = length
+  }
+
   perimeter(){
     return this.length * 4
+  }
+  area(){
+    return this.length * this.length
   }
 }
 
 class Polygon {
   constructor(length, sides) {
+    this.length = length
     this.sides = sides
   }
   perimeter(){
--- test/indexTest.js
+++ test/indexTest.js
@@ -2,11 +2,22 @@ describe("Shape", () => {
   it("has a Shape class", () => {
     expect(Shape).to.exist
   })
+  it("Shape has a length property", () => {
+    var shape = new Shape(5)
+
+    expect(shape.length).to.equal(5)
+  })
 })
 
 describe("Triangle", () => {
   it("has a Triangle class", () => {
     expect(Triangle).to.exist
+  })
+
+  it("Shape calculates the perimeter", () => {
+    var triangle = new Triangle(5)
+
+    expect(triangle.perimeter()).to.equal(15)
   })
 })
 
@@ -15,11 +26,29 @@ describe("Square", () => {
   it("has a Square class", () => {
     expect(Square).to.exist
   })
+
+  it("Square calculates the perimeter", () => {
+    var square = new Square(5)
+
+    expect(square.perimeter()).to.equal(20)
+  })
+
+  it("Square calculates the area", () => {
+    var square = new Square(5)
+
+    expect(square.area()).to.equal(25)
+  })
 })
 
 
 describe("Polygon", () => {
   it("has a Polygon class", () => {
     expect(Polygon).to.exist
+  })
+
+  it("Polygon calculates the perimeter", () => {
+    var polygon = new Polygon(5, 10)
+
+    expect(polygon.perimeter()).to.equal(50)
   })
 })
