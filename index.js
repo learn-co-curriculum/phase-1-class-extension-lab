39de5c9110c44c788e6c9c960dcc29933909cf7f
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-12-05 16:46:37 -0500

    Adds answer

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-class Shape {
-  constructor(length) {
-    this.length = length
-  }
-}
-
-class Triangle {
-  constructor(length) {
-    this.length = length
-  }
-
-  perimeter(){
-    return this.length * 3
-  }
-}
-
-class Square {
-  constructor(length) {
-    this.length = length
-  }
-
-  perimeter(){
-    return this.length * 4
-  }
-  area(){
-    return this.length * this.length
-  }
-}
-
 class Polygon {
-  constructor(length, sides) {
-    this.length = length
+  constructor(sides) {
     this.sides = sides
+    this.count = this.sides.length
   }
+
+  get getCount() {
+    return this.sides.length
+  }
+
   perimeter(){
-    return this.length * this.sides
+    if(!Array.isArray(this.sides)) return;
+    let sum = 0;
+    for (var int of this.sides) {
+      sum+=int
+    }
+    return sum
+  }
+}
+
+class Triangle extends Polygon{
+
+  isValid() {
+    if(!Array.isArray(this.sides)) return;
+    if(this.count !== 3) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    return ((side1+side2)>side3) && ((side1+side3)>side2) && ((side2+side3)>side1)
+  }
+}
+
+class Square extends Polygon {
+
+  isValid() {
+    if(!Array.isArray(this.sides)) return;
+    if(this.count !== 4) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    let side4 = this.sides[3]
+    if(side1 === side2 && side1 === side3 && side1 === side4) return;
+  }
+
+  area(){
+    if(!Array.isArray(this.sides)) return;
+    if(this.count !== 4) return;
+    return this.sides[0] * this.sides[0]
   }
 }
