77d769ba9b84b8cbc6b5e67f2bff86654b88c58c
Kellye Greene <kellye.greene@flatironschool.com>
2018-12-07 11:33:07 -0500

    Removes comments from tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -16,19 +16,15 @@ describe( "Polygon", () => {
     expect( polygon.sides ).to.have.all.members( [ 5, 5, 5 ] )
   } )
 
-  // count should be a getter
   it( "Polygon has a count getter that counts the sides of the polygon", () => {
     expect( polygon.count ).to.exist
     expect( polygon.count ).to.eq( 3 )
   } )
-
-  // this is an interesting thing - what happens if count is set to more than the number of sides?
-  // we could definitely include this but maybe we just keep it simple and use a count getter only
+  
   it( "Polygon has a count setter method that sets the count of the sides of the polygon", () => {
     expect( polygon.count ).to.eq( 3 )
   } )
 
-  //perimeter should be a getter
   it( "Polygon has a perimeter getter that calculates perimeter", () => {
     expect( polygon.perimeter ).to.eq( 15 )
   } )
@@ -45,7 +41,6 @@ describe( "Triangle", () => {
 
     expect( triangle.count ).to.eq( 3 )
 
-    //isValid can be a getter too
     expect( triangle.isValid ).to.be.true
     expect( triangle2.isValid ).to.be.false
   } )
@@ -53,8 +48,6 @@ describe( "Triangle", () => {
   it( "has a perimeter getter inherited from Polygon", () => {
     expect( polygon.perimeter ).to.eq( 15 )
   } )
-
-  //should also include check for inherited count getter method
 } )
 
 
@@ -69,12 +62,9 @@ describe( "Square", () => {
     expect( square.perimeter ).to.eq( 20 )
   } )
 
-  // check for count getter inheritance as well
-
   it( "calculates the area", () => {
     var square = new Square( [ 5, 5, 5, 5 ] )
 
-    //to getter
     expect( square.area ).to.eq( 25 )
   } )
 
@@ -82,7 +72,6 @@ describe( "Square", () => {
     var square = new Square( [ 5, 5, 5, 5 ] )
     var square2 = new Square( [ 5, 4, 3, 2 ] )
 
-    //getters:
     expect( square.count ).to.eq( 4 )
     expect( square.isValid ).to.be.true
     expect( square2.isValid ).to.be.false
