ef65f09560729a9382045478baef837d59e8154a
Kellye Greene <kellye.greene@flatironschool.com>
2018-12-07 11:37:47 -0500

    Updates solution

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class Triangle extends Polygon{
     let side1 = this.sides[0]
     let side2 = this.sides[1]
     let side3 = this.sides[2]
-    return ((side1+side2)>side3) && ((side1+side3)>side2) && ((side2+side3)>side1)
+    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
   }
 }
 
