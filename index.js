4c3dd1b4401913f9e6a33efb79f4c98a6fbca8c5
Badr Nasher <badrnasher@gmail.com>
2023-04-07 03:38:04 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+class Polygon {
+  constructor(arr) {
+    this.arr = arr;
+  }
+  get countSides() {
+    return this.arr.length;
+  }
+  get perimeter() {
+    return this.arr.reduce((acc, curr) => acc + curr, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides !== 3) return false;
+    const [a, b, c] = this.arr;
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.countSides !== 4) return false;
+    const [a, b, c, d] = this.arr;
+    return a === b && b === c && c === d;
+  }
+
+  get area() {
+    return this.arr[0] ** 2;
+  }
+}
